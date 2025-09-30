45e07d2cc67d6975f515e2079c2e84373dfabeaa
Kauã ravi <ravisenac@gmail.com>
2025-09-30 15:28:04 -0300

    Add files via upload

---
+++ Ravi-senac/PRATICA05/script.js
@@ -0,0 +1,18 @@
+//arrays-> vetores, matrizes
+//Estruturá de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maçã", "Melancia", "Abacaxi"]; //array tamanho 4
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(10,2,6,7);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Ravi";
+console.log(arrayVazio);
+
+//Tamanho de array -> length
+let dados = [32,4,80,6];//tamanho 5 -> legth 5
+console.log(dados.length);
